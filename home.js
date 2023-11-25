2279c6dde900eb104078488f255f5dd631518bcf
Michael Gallahan <gallahanm@gmail.com>
2023-11-25 01:12:14 -0500

    Fixed link to reference http instead of https

--- home.js
+++ home.js
@@ -1,5 +1,5 @@
 let carefreeButton = document.querySelector('.project-card');
 console.log(carefreeButton);
 carefreeButton.addEventListener('click', e => {
-  window.open('//carefree.michaelgallahan.com', '_self');
+  window.open('http://carefree.michaelgallahan.com', '_self');
 });
