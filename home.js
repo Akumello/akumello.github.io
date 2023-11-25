3f1d72b693f49ba9d79ec991db67a2236a89a32a
Michael Gallahan <gallahanm@gmail.com>
2023-11-25 00:29:38 -0500

    Added projects section and link to carefree site

---
+++ home.js
@@ -0,0 +1,5 @@
+let carefreeButton = document.querySelector('.project-card');
+console.log(carefreeButton);
+carefreeButton.addEventListener('click', e => {
+  window.open('//carefree.michaelgallahan.com', '_self');
+});
