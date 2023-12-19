7b3243189cd95af1c1427d8d277ab43ed846775a
Michael Gallahan <gallahanm@gmail.com>
2023-12-19 15:53:15 -0800

    Add js to animate home image filling the screen, then lifting up

--- home.js
+++ home.js
@@ -1,6 +1,15 @@
 let carefreeButton = document.querySelector('#carefree');
 let linksBar = document.querySelector('.links');
 let links = linksBar.querySelectorAll('.header-link');
+let rightPane = document.querySelector('.right-pane');
+let leftPane = document.querySelector('.left-pane');
+
+let curScrollX = 0;
+let curScrollY = 0;
+
+function clamp(number, min, max) {
+  return Math.max(min, Math.min(number, max));
+}
 
 //Execute
 function setLinkBarToCenter() {
@@ -19,26 +28,34 @@ function setLinkBold() {
 }
 
 addEventListener('resize', e => {
-  setLinkBarToCenter();
+  //setLinkBarToCenter();
 });
 
 carefreeButton.addEventListener('click', e => {
   window.open('http://carefree.michaelgallahan.com', '_self');
 });
 
-function disableScrollX() {  
-  // Get the current page scroll position in the horizontal direction 
-  scrollLeft = window.scrollX || document.documentElement.scrollLeft;
-  
-  
-  // If horizontal scroll is attempted,
-  // Set the original value
-  window.onscroll = function() {
-  window.scrollTo(scrollLeft, window.scrollY);
-  };
-}
+window.onwheel = e => {
+  curScrollY += e.deltaY;
+  let newLeft = clamp(50 * (1 - curScrollY / 2000), 0, 50);
+  let newBottom = clamp(100 * ((curScrollY - 2000) / 2000), 0, 105);
+  if (curScrollY < 0)
+    curScrollY = 0;
+  if (newLeft < 42)
+    leftPane.style.opacity = 0;  
+  else
+    leftPane.style.opacity = 1;
 
-// setLinkBarToCenter();
+  if (curScrollY > 2000)
+    document.body.style.backgroundColor = '#2B2C28';
+  else
+    document.body.style.backgroundColor = '#E5E4E2';
+
+  console.log(`y: ${curScrollY}`);
+
+  let bottomText = (newBottom > 5) ? `bottom: ${newBottom - 5}%;` : '';
+  rightPane.setAttribute('style', `left: ${newLeft}%; ${bottomText}`);
+};
+
 setLinkBold();
-disableScrollX();
 linksBar.setAttribute('style', `opacity: 1;`);
