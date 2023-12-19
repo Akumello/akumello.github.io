a5105ecfc6a938cb5c1f667e3facabfa65544343
Michael Gallahan <gallahanm@gmail.com>
2023-12-18 21:16:05 -0800

    Redesigned site

--- home.js
+++ home.js
@@ -1,5 +1,30 @@
-let carefreeButton = document.querySelector('.project-card');
-console.log(carefreeButton);
+let carefreeButton = document.querySelector('#carefree');
+let linksBar = document.querySelector('.links');
+let links = linksBar.querySelectorAll('.header-link');
+
+//Execute
+function setLinkBarToCenter() {
+  let screenWidth = window.innerWidth;
+  let linksStyle = getComputedStyle(linksBar);
+  let linkPercent = (1 - (screenWidth - parseFloat(linksStyle.width)) / screenWidth) * 100;
+  let leftPercent = 50 - (linkPercent / 2);
+  console.log(screenWidth);
+  linksBar.setAttribute('style', `left: ${leftPercent}%; opacity: 1;`);
+}
+
+function setLinkBold() {
+  console.log(links);
+  links[0].classList.remove('header-link');
+  links[0].classList.add('header-link-selected');
+}
+
+addEventListener('resize', e => {
+  setLinkBarToCenter();
+});
+
 carefreeButton.addEventListener('click', e => {
   window.open('http://carefree.michaelgallahan.com', '_self');
 });
+
+setLinkBarToCenter();
+setLinkBold();
