f6459073588cdf539cb8228a12c2f307aa9d12e8
Michael Gallahan <gallahanm@gmail.com>
2024-01-14 06:54:46 -0800

    Added projects pane with carefree demo video. Added tweaks to animations.

--- home.js
+++ home.js
@@ -3,6 +3,15 @@ let linksBar = document.querySelector('.links');
 let links = linksBar.querySelectorAll('.header-link');
 let rightPane = document.querySelector('.right-pane');
 let leftPane = document.querySelector('.left-pane');
+let projects = document.querySelector('.projects');
+const Areas = {
+  Home: 0,
+  HomeImage: 1,
+  About: 2
+};
+let currentArea = Areas.Home;
+let headerCurSelected = Areas.Home;
+let carefreeVideo = document.querySelector('#carefree_video');
 
 let curScrollX = 0;
 let curScrollY = 0;
@@ -21,10 +30,12 @@ function setLinkBarToCenter() {
   linksBar.setAttribute('style', `left: ${leftPercent}%; opacity: 1;`);
 }
 
-function setLinkBold() {
-  console.log(links);
-  links[0].classList.remove('header-link');
-  links[0].classList.add('header-link-selected');
+function setLinkBold(area) {
+  links[headerCurSelected].classList.add('header-link');
+  links[headerCurSelected].classList.remove('header-link-selected');
+  headerCurSelected = area;
+  links[area].classList.remove('header-link');
+  links[area].classList.add('header-link-selected');
 }
 
 addEventListener('resize', e => {
@@ -35,27 +46,67 @@ carefreeButton.addEventListener('click', e => {
   window.open('http://carefree.michaelgallahan.com', '_self');
 });
 
+function scroll(scrollDelta) {
+  // Keep track of the users current position
+  curScrollY += scrollDelta;
+
+  // How far the user must scroll to finish home animation and dismiss the home image
+  let homeAnimDepth = 1000;
+  let imgDismissDepth = 2000;
+  let imgStickDuration = 10;
+  let titleFadeLocation = 42;
+
+  switch(true) {
+    case curScrollY < 0:
+      curScrollY = 0;
+    case curScrollY < homeAnimDepth:                     // Image scrolling horizontal
+      // Perform these lines one time upon entry into Home area
+      if(!(currentArea == Areas.Home)) {
+        leftPane.style.display = 'block';
+        projects.style.display = 'none';
+        document.body.style.backgroundColor = getComputedStyle(document.documentElement).getPropertyValue('--light');
+      }
+      currentArea = Areas.Home;
+      break;
+    case curScrollY < (homeAnimDepth + imgDismissDepth): // Image scrolling vertical
+      // Perform these lines one time upon entry into HomeImage area
+      if(!(currentArea == Areas.HomeImage)) {
+        leftPane.style.display = 'none';
+        document.body.style.backgroundColor = getComputedStyle(document.documentElement).getPropertyValue('--dark-accent');
+        projects.style.display = 'block';
+        projects.style.opacity = 0;
+      }
+      currentArea = Areas.HomeImage;
+      break;
+    default:                                             // Image no longer visible
+      break;
+  }
+
+  
+
+  let newLeft = clamp(50 * (1 - curScrollY / homeAnimDepth), 0, 50);
+  let newBottom = clamp(100 * ((curScrollY - homeAnimDepth) / imgDismissDepth), 0, 100 + imgStickDuration);
+
+  leftPane.style.opacity = (newLeft < titleFadeLocation) ? 0 : 1;
+
+  rightPane.style.left = newLeft + '%';
+  rightPane.style.bottom = (newBottom > imgStickDuration) ? newBottom - imgStickDuration + '%' : '';
+
+  // Display projects pane when home image is halfway dismissed
+  if (curScrollY > (homeAnimDepth + (imgDismissDepth / 2))) {
+    setLinkBold(Areas.HomeImage);
+    projects.style.opacity = 1;
+    carefreeVideo.play();
+  }
+  else {
+    setLinkBold(Areas.Home);
+    projects.style.opacity = 0;
+  }
+}
+
 window.onwheel = e => {
-  curScrollY += e.deltaY;
-  let newLeft = clamp(50 * (1 - curScrollY / 2000), 0, 50);
-  let newBottom = clamp(100 * ((curScrollY - 2000) / 2000), 0, 105);
-  if (curScrollY < 0)
-    curScrollY = 0;
-  if (newLeft < 42)
-    leftPane.style.opacity = 0;  
-  else
-    leftPane.style.opacity = 1;
-
-  if (curScrollY > 2000)
-    document.body.style.backgroundColor = '#2B2C28';
-  else
-    document.body.style.backgroundColor = '#E5E4E2';
-
-  console.log(`y: ${curScrollY}`);
-
-  let bottomText = (newBottom > 5) ? `bottom: ${newBottom - 5}%;` : '';
-  rightPane.setAttribute('style', `left: ${newLeft}%; ${bottomText}`);
+  scroll(e.deltaY);
 };
 
-setLinkBold();
-linksBar.setAttribute('style', `opacity: 1;`);
+setLinkBold(Areas.Home);
+linksBar.setAttribute('style', 'opacity: 1;');
